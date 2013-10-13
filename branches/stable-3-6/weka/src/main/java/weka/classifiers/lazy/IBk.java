53abb6c47b83f81b954617a7896150be23bba1cf
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-10-13 20:46:50 +0000

    Updated the tip text for the cross-validate option.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10069 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/stable-3-6/weka/src/main/java/weka/classifiers/lazy/IBk.java
+++ branches/stable-3-6/weka/src/main/java/weka/classifiers/lazy/IBk.java
@@ -385,8 +385,9 @@ public class IBk
    */
   public String crossValidateTipText() {
 
-    return "Whether hold-one-out cross-validation will be used " +
-      "to select the best k value.";
+    return "Whether hold-one-out cross-validation will be used to " +
+      "select the best k value between 1 and the value specified as " +
+      "the KNN parameter.";
   }
   
   /**
