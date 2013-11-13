74b187e19e5eaba75c696fcdc6daa2de370e1d89
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-11-13 03:29:58 +0000

    Fixed a bug in the specification of event types produced by Appender and in the routine that determines whether a particular event type can be generated at a given point in time.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10152 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/waikato/weka/src/main/java/weka/gui/beans/AppenderBeanInfo.java
+++ branches/waikato/weka/src/main/java/weka/gui/beans/AppenderBeanInfo.java
@@ -15,7 +15,7 @@
 
 /*
  *    AppenderBeanInfo.java
- *    Copyright (C) 2011 University of Waikato, Hamilton, New Zealand
+ *    Copyright (C) 2011-2012 University of Waikato, Hamilton, New Zealand
  *
  */
 
@@ -31,31 +31,20 @@ import java.beans.SimpleBeanInfo;
  * @version $Revision$
  */
 public class AppenderBeanInfo extends SimpleBeanInfo {
-  
+
   /**
    * Returns the event set descriptors
-   *
+   * 
    * @return an <code>EventSetDescriptor[]</code> value
    */
-  public EventSetDescriptor [] getEventSetDescriptors() {
+  @Override
+  public EventSetDescriptor[] getEventSetDescriptors() {
     try {
-      EventSetDescriptor [] esds = 
-      { new EventSetDescriptor(DataSource.class, 
-                               "dataSet", 
-                               DataSourceListener.class, 
-                               "acceptDataSet"),
-        new EventSetDescriptor(DataSource.class, 
-                               "instance", 
-                               InstanceListener.class, 
-                               "acceptInstance"),
-        new EventSetDescriptor(TrainingSetProducer.class, 
-                               "trainingSet", 
-                               TrainingSetListener.class, 
-                               "acceptTrainingSet"),
-        new EventSetDescriptor(TestSetProducer.class, 
-                               "testSet", 
-                               TestSetListener.class, 
-                               "acceptTestSet")  };
+      EventSetDescriptor[] esds = {
+        new EventSetDescriptor(DataSource.class, "dataSet",
+          DataSourceListener.class, "acceptDataSet"),
+        new EventSetDescriptor(DataSource.class, "instance",
+          InstanceListener.class, "acceptInstance"), };
       return esds;
     } catch (Exception ex) {
       ex.printStackTrace();
