1a27452a9e8dba939c2620a1c426c72ccc110aef
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-12-11 01:02:43 +0000

    Javadoc update
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10388 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/waikato/weka/src/main/java/weka/gui/beans/SubstringLabelerBeanInfo.java
+++ branches/waikato/weka/src/main/java/weka/gui/beans/SubstringLabelerBeanInfo.java
@@ -15,7 +15,7 @@
 
 /*
  *    SubstringLabelerBeanInfo.java
- *    Copyright (C) 2011 University of Waikato, Hamilton, New Zealand
+ *    Copyright (C) 2011-2012 University of Waikato, Hamilton, New Zealand
  *
  */
 
