bfe7699fcc3590a40ff4686a812ae8f03dc3f799
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-03-11 00:52:26 +0000

    Now implements Serializable.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10500 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/waikato/weka/src/main/java/weka/classifiers/trees/ht/Split.java
+++ branches/waikato/weka/src/main/java/weka/classifiers/trees/ht/Split.java
@@ -21,6 +21,7 @@
 
 package weka.classifiers.trees.ht;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +34,7 @@ import weka.core.Instance;
  * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
  * @version $Revision$
  */
-public abstract class Split {
+public abstract class Split implements Serializable {
 
   /** name(s) of attribute(s) involved in the split */
   protected List<String> m_splitAttNames = new ArrayList<String>();
