9ffc22d1f024512315a2aee93afd35c75affc257
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-04-01 01:49:44 +0000

    From Eibe: Handling of sparse instances is now implemented correctly
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10578 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/stable-3-6/weka/src/main/java/weka/clusterers/forOPTICSAndDBScan/DataObjects/ManhattanDataObject.java
+++ branches/stable-3-6/weka/src/main/java/weka/clusterers/forOPTICSAndDBScan/DataObjects/ManhattanDataObject.java
@@ -22,14 +22,14 @@
 
 package weka.clusterers.forOPTICSAndDBScan.DataObjects;
 
-import java.io.Serializable;
-
 import weka.clusterers.forOPTICSAndDBScan.Databases.Database;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.RevisionHandler;
 import weka.core.RevisionUtils;
 import weka.core.Utils;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -39,276 +39,315 @@ import weka.core.Utils;
  * Time: 5:50:22 PM <br/>
  * $ Revision 1.4 $ <br/>
  * </p>
- * 
+ *
  * @author Matthias Schubert (schubert@dbs.ifi.lmu.de)
  * @author Zhanna Melnikova-Albrecht (melnikov@cip.ifi.lmu.de)
  * @author Rainer Holzmann (holzmann@cip.ifi.lmu.de)
- * @version $Revision: 9420 $
+ * @version $Revision: 10571 $
  */
-public class ManhattanDataObject implements DataObject, Serializable,
-    RevisionHandler {
+public class ManhattanDataObject
+    implements DataObject, Serializable, RevisionHandler {
 
-  /** for serialization */
-  private static final long serialVersionUID = -3417720553766544582L;
+    /** for serialization */
+    private static final long serialVersionUID = -3417720553766544582L;
 
-  /**
-   * Holds the original instance
-   */
-  private final Instance instance;
+    /**
+     * Holds the original instance
+     */
+    private Instance instance;
 
-  /**
-   * Holds the (unique) key that is associated with this DataObject
-   */
-  private String key;
+    /**
+     * Holds the (unique) key that is associated with this DataObject
+     */
+    private String key;
 
-  /**
-   * Holds the ID of the cluster, to which this DataObject is assigned
-   */
-  private int clusterID;
+    /**
+     * Holds the ID of the cluster, to which this DataObject is assigned
+     */
+    private int clusterID;
 
-  /**
-   * Holds the status for this DataObject (true, if it has been processed, else
-   * false)
-   */
-  private boolean processed;
+    /**
+     * Holds the status for this DataObject (true, if it has been processed, else false)
+     */
+    private boolean processed;
 
-  /**
-   * Holds the coreDistance for this DataObject
-   */
-  private double c_dist;
+    /**
+     * Holds the coreDistance for this DataObject
+     */
+    private double c_dist;
 
-  /**
-   * Holds the reachabilityDistance for this DataObject
-   */
-  private double r_dist;
+    /**
+     * Holds the reachabilityDistance for this DataObject
+     */
+    private double r_dist;
 
-  /**
-   * Holds the database, that is the keeper of this DataObject
-   */
-  private final Database database;
+    /**
+     * Holds the database, that is the keeper of this DataObject
+     */
+    private Database database;
 
-  // *****************************************************************************************************************
-  // constructors
-  // *****************************************************************************************************************
+    // *****************************************************************************************************************
+    // constructors
+    // *****************************************************************************************************************
 
-  /**
-   * Constructs a new DataObject. The original instance is kept as
-   * instance-variable
-   * 
-   * @param originalInstance the original instance
-   */
-  public ManhattanDataObject(Instance originalInstance, String key,
-      Database database) {
-    this.database = database;
-    this.key = key;
-    instance = originalInstance;
-    clusterID = DataObject.UNCLASSIFIED;
-    processed = false;
-    c_dist = DataObject.UNDEFINED;
-    r_dist = DataObject.UNDEFINED;
-  }
-
-  // *****************************************************************************************************************
-  // methods
-  // *****************************************************************************************************************
-
-  /**
-   * Compares two DataObjects in respect to their attribute-values
-   * 
-   * @param dataObject The DataObject, that is compared with this.dataObject;
-   *          now assumed to be of the same type and with the same structure
-   * @return Returns true, if the DataObjects correspond in each value, else
-   *         returns false
-   */
-  public boolean equals(DataObject dataObject) {
-    if (this == dataObject)
-      return true;
-
-    for (int i = 0; i < getInstance().numValues(); i++) {
-      double i_value_Instance_1 = getInstance().valueSparse(i);
-      double i_value_Instance_2 = dataObject.getInstance().valueSparse(i);
-
-      if (i_value_Instance_1 != i_value_Instance_2)
-        return false;
+    /**
+     * Constructs a new DataObject. The original instance is kept as instance-variable
+     * @param originalInstance the original instance
+     */
+    public ManhattanDataObject(Instance originalInstance, String key, Database database) {
+        this.database = database;
+        this.key = key;
+        instance = originalInstance;
+        clusterID = DataObject.UNCLASSIFIED;
+        processed = false;
+        c_dist = DataObject.UNDEFINED;
+        r_dist = DataObject.UNDEFINED;
     }
-    return true;
-  }
 
-  /**
-   * Calculates the manhattan-distance between dataObject and this.dataObject
-   * 
-   * @param dataObject The DataObject, that is used for distance-calculation
-   *          with this.dataObject now assumed to be of the same type and with
-   *          the same structure
-   * @return double-value The manhattan-distance between dataObject and
-   *         this.dataObject NaN, if the computation could not be performed
-   */
-  public double distance(DataObject dataObject) {
-    double dist = 0.0;
+    // *****************************************************************************************************************
+    // methods
+    // *****************************************************************************************************************
 
-    for (int i = 0; i < getInstance().numValues(); i++) {
-      double cDistance = computeDistance(getInstance().index(i), getInstance()
-          .valueSparse(i), dataObject.getInstance().valueSparse(i));
-      dist += Math.abs(cDistance);
-    }
-    return dist;
-  }
 
-  /**
-   * Performs manhattan-distance-calculation between two given values
-   * 
-   * @param index of the attribute within the DataObject's instance
-   * @param v value_1
-   * @param v1 value_2
-   * @return double norm-distance between value_1 and value_2
-   */
-  private double computeDistance(int index, double v, double v1) {
-    switch (getInstance().attribute(index).type()) {
-    case Attribute.NOMINAL:
-      return (Instance.isMissingValue(v) || Instance.isMissingValue(v1) || ((int) v != (int) v1)) ? 1
-          : 0;
+    /**
+     * Compares two DataObjects in respect to their attribute-values
+     * @param dataObject The DataObject, that is compared with this.dataObject;
+     *        now assumed to be of the same type and with the same structure
+     * @return Returns true, if the DataObjects correspond in each value, else returns false
+     */
+    public boolean equals(DataObject dataObject) {
+      if (this == dataObject) return true;
 
-    case Attribute.NUMERIC:
-      if (Instance.isMissingValue(v) || Instance.isMissingValue(v1)) {
-        if (Instance.isMissingValue(v) && Instance.isMissingValue(v1))
-          return 1;
-        else {
-          return (Instance.isMissingValue(v)) ? norm(v1, index)
-              : norm(v, index);
+      Instance firstInstance = getInstance();
+      Instance secondInstance = dataObject.getInstance(); 
+      int firstNumValues = firstInstance.numValues();
+      int secondNumValues = secondInstance.numValues();
+      int numAttributes = firstInstance.numAttributes();
+
+      int firstI, secondI;
+      for (int p1 = 0, p2 = 0; p1 < firstNumValues || p2 < secondNumValues;) {
+        if (p1 >= firstNumValues) {
+          firstI = numAttributes;
+        } else {
+          firstI = firstInstance.index(p1);
         }
-      } else
-        return norm(v, index) - norm(v1, index);
 
-    default:
-      return 0;
+        if (p2 >= secondNumValues) {
+          secondI = numAttributes;
+        } else {
+          secondI = secondInstance.index(p2);
+        }
+
+        if (firstI == secondI) {
+          if (firstInstance.valueSparse(p1) != secondInstance.valueSparse(p2)) {
+            return false;
+          }
+          p1++;
+          p2++;
+        } else if (firstI > secondI) {
+          if (0 != secondInstance.valueSparse(p2)) {
+            return false;
+          }
+          p2++;
+        } else {
+          if (0 != firstInstance.valueSparse(p1)) {
+            return false;
+          }
+          p1++;
+        }
+      }
+      return true;
     }
-  }
 
-  /**
-   * Normalizes a given value of a numeric attribute.
-   * 
-   * @param x the value to be normalized
-   * @param i the attribute's index
-   */
-  private double norm(double x, int i) {
-    if (Double.isNaN(database.getAttributeMinValues()[i])
-        || Utils.eq(database.getAttributeMaxValues()[i],
-            database.getAttributeMinValues()[i])) {
-      return 0;
-    } else {
-      return (x - database.getAttributeMinValues()[i])
-          / (database.getAttributeMaxValues()[i] - database
-              .getAttributeMinValues()[i]);
+    /**
+     * Calculates the manhattan-distance between dataObject and this.dataObject
+     * @param dataObject The DataObject, that is used for distance-calculation with this.dataObject
+     *        now assumed to be of the same type and with the same structure
+     * @return double-value The manhattan-distance between dataObject and this.dataObject
+     *                      NaN, if the computation could not be performed
+     */
+    public double distance(DataObject dataObject) {
+      double dist = 0.0;
+
+      Instance firstInstance = getInstance();
+      Instance secondInstance = dataObject.getInstance(); 
+      int firstNumValues = firstInstance.numValues();
+      int secondNumValues = secondInstance.numValues();
+      int numAttributes = firstInstance.numAttributes();
+
+      int firstI, secondI;
+      for (int p1 = 0, p2 = 0; p1 < firstNumValues || p2 < secondNumValues;) {
+        if (p1 >= firstNumValues) {
+          firstI = numAttributes;
+        } else {
+          firstI = firstInstance.index(p1);
+        }
+
+        if (p2 >= secondNumValues) {
+          secondI = numAttributes;
+        } else {
+          secondI = secondInstance.index(p2);
+        }
+
+        double cDistance = 0;
+        if (firstI == secondI) {
+          cDistance = computeDistance(firstI, firstInstance.valueSparse(p1),
+                                      secondInstance.valueSparse(p2));
+          p1++;
+          p2++;
+        } else if (firstI > secondI) {
+          cDistance = computeDistance(secondI, 0, secondInstance.valueSparse(p2));
+          p2++;
+        } else {
+          cDistance = computeDistance(firstI, firstInstance.valueSparse(p1), 0);
+          p1++;
+        }
+        dist += Math.abs(cDistance);
+      }
+      return dist;
     }
-  }
 
-  /**
-   * Returns the original instance
-   * 
-   * @return originalInstance
-   */
-  public Instance getInstance() {
-    return instance;
-  }
+    /**
+     * Performs manhattan-distance-calculation between two given values
+     * @param index of the attribute within the DataObject's instance
+     * @param v value_1
+     * @param v1 value_2
+     * @return double norm-distance between value_1 and value_2
+     */
+    private double computeDistance(int index, double v, double v1) {
+        switch (getInstance().attribute(index).type()) {
+            case Attribute.NOMINAL:
+                return (Instance.isMissingValue(v) || Instance.isMissingValue(v1)
+                        || ((int) v != (int) v1)) ? 1 : 0;
 
-  /**
-   * Returns the key for this DataObject
-   * 
-   * @return key
-   */
-  public String getKey() {
-    return key;
-  }
+            case Attribute.NUMERIC:
+                if (Instance.isMissingValue(v) || Instance.isMissingValue(v1)) {
+                    if (Instance.isMissingValue(v) && Instance.isMissingValue(v1))
+                        return 1;
+                    else {
+                        return (Instance.isMissingValue(v)) ? norm(v1, index)
+                                : norm(v, index);
+                    }
+                } else
+                    return norm(v, index) - norm(v1, index);
 
-  /**
-   * Sets the key for this DataObject
-   * 
-   * @param key The key is represented as string
-   */
-  public void setKey(String key) {
-    this.key = key;
-  }
+            default:
+                return 0;
+        }
+    }
 
-  /**
-   * Sets the clusterID (cluster), to which this DataObject belongs to
-   * 
-   * @param clusterID Number of the Cluster
-   */
-  public void setClusterLabel(int clusterID) {
-    this.clusterID = clusterID;
-  }
+    /**
+     * Normalizes a given value of a numeric attribute.
+     *
+     * @param x the value to be normalized
+     * @param i the attribute's index
+     */
+    private double norm(double x, int i) {
+        if (Double.isNaN(database.getAttributeMinValues()[i])
+                || Utils.eq(database.getAttributeMaxValues()[i], database.getAttributeMinValues()[i])) {
+            return 0;
+        } else {
+            return (x - database.getAttributeMinValues()[i]) /
+                    (database.getAttributeMaxValues()[i] - database.getAttributeMinValues()[i]);
+        }
+    }
 
-  /**
-   * Returns the clusterID, to which this DataObject belongs to
-   * 
-   * @return clusterID
-   */
-  public int getClusterLabel() {
-    return clusterID;
-  }
+    /**
+     * Returns the original instance
+     * @return originalInstance
+     */
+    public Instance getInstance() {
+        return instance;
+    }
 
-  /**
-   * Marks this dataObject as processed
-   * 
-   * @param processed True, if the DataObject has been already processed, false
-   *          else
-   */
-  public void setProcessed(boolean processed) {
-    this.processed = processed;
-  }
+    /**
+     * Returns the key for this DataObject
+     * @return key
+     */
+    public String getKey() {
+        return key;
+    }
 
-  /**
-   * Gives information about the status of a dataObject
-   * 
-   * @return True, if this dataObject has been processed, else false
-   */
-  public boolean isProcessed() {
-    return processed;
-  }
+    /**
+     * Sets the key for this DataObject
+     * @param key The key is represented as string
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
 
-  /**
-   * Sets a new coreDistance for this dataObject
-   * 
-   * @param c_dist coreDistance
-   */
-  public void setCoreDistance(double c_dist) {
-    this.c_dist = c_dist;
-  }
+    /**
+     * Sets the clusterID (cluster), to which this DataObject belongs to
+     * @param clusterID Number of the Cluster
+     */
+    public void setClusterLabel(int clusterID) {
+        this.clusterID = clusterID;
+    }
 
-  /**
-   * Returns the coreDistance for this dataObject
-   * 
-   * @return coreDistance
-   */
-  public double getCoreDistance() {
-    return c_dist;
-  }
+    /**
+     * Returns the clusterID, to which this DataObject belongs to
+     * @return clusterID
+     */
+    public int getClusterLabel() {
+        return clusterID;
+    }
 
-  /**
-   * Sets a new reachability-distance for this dataObject
-   */
-  public void setReachabilityDistance(double r_dist) {
-    this.r_dist = r_dist;
-  }
+    /**
+     * Marks this dataObject as processed
+     * @param processed True, if the DataObject has been already processed, false else
+     */
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
 
-  /**
-   * Returns the reachabilityDistance for this dataObject
-   */
-  public double getReachabilityDistance() {
-    return r_dist;
-  }
+    /**
+     * Gives information about the status of a dataObject
+     * @return True, if this dataObject has been processed, else false
+     */
+    public boolean isProcessed() {
+        return processed;
+    }
 
-  @Override
-  public String toString() {
-    return instance.toString();
-  }
+    /**
+     * Sets a new coreDistance for this dataObject
+     * @param c_dist coreDistance
+     */
+    public void setCoreDistance(double c_dist) {
+        this.c_dist = c_dist;
+    }
 
-  /**
-   * Returns the revision string.
-   * 
-   * @return the revision
-   */
-  public String getRevision() {
-    return RevisionUtils.extract("$Revision: 9420 $");
-  }
+    /**
+     * Returns the coreDistance for this dataObject
+     * @return coreDistance
+     */
+    public double getCoreDistance() {
+        return c_dist;
+    }
+
+    /**
+     * Sets a new reachability-distance for this dataObject
+     */
+    public void setReachabilityDistance(double r_dist) {
+        this.r_dist = r_dist;
+    }
+
+    /**
+     * Returns the reachabilityDistance for this dataObject
+     */
+    public double getReachabilityDistance() {
+        return r_dist;
+    }
+
+    public String toString() {
+        return instance.toString();
+    }
+    
+    /**
+     * Returns the revision string.
+     * 
+     * @return		the revision
+     */
+    public String getRevision() {
+      return RevisionUtils.extract("$Revision: 10571 $");
+    }
 }
