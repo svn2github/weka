396a714f545fefac7e31ad42da9e461bf384e4fc
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-05-11 23:15:10 +0000

    Fixed a doc typo.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@10833 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/waikato/weka/src/main/java/weka/clusterers/UpdateableClusterer.java
+++ branches/waikato/weka/src/main/java/weka/clusterers/UpdateableClusterer.java
@@ -23,24 +23,24 @@ package weka.clusterers;
 import weka.core.Instance;
 
 /**
- * Interface to incremental cluster models that can learn using one instance 
- * at a time.
+ * Interface to incremental cluster models that can learn using one instance at
+ * a time.
  * 
- * @author  FracPete (fracpete at waikato dot ac dot nz)
+ * @author FracPete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
  */
 public interface UpdateableClusterer {
 
   /**
    * Adds an instance to the clusterer.
-   *
+   * 
    * @param newInstance the instance to be added
-   * @throws Exception 	if something goes wrong
+   * @throws Exception if something goes wrong
    */
   public void updateClusterer(Instance newInstance) throws Exception;
 
   /**
-   * Singals the end of the updating.
+   * Signals the end of the updating.
    */
   public void updateFinished();
 }
