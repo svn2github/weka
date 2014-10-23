ae59fc5aa9afda40f707aec997c237402940a4d7
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-10-23 07:25:40 +0000

    Fixed some problems identified by FindBugs. Nothing particularly serious.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka@11250 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- branches/waikato/weka/src/main/java/weka/core/Tee.java
+++ branches/waikato/weka/src/main/java/weka/core/Tee.java
@@ -15,7 +15,7 @@
 
 /*
  * Tee.java
- * Copyright (C) 2005 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2005-2012 University of Waikato, Hamilton, New Zealand
  *
  */
 
@@ -70,7 +70,7 @@ public class Tee
    * initializes the object, with a default printstream.
    */
   public Tee() {
-    this(null);
+    this(System.out);
   }
 
   /**
